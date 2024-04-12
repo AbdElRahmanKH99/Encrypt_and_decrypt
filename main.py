31ab6b717200738bc1282f4478e453ba2c76fdce
AbdElRahmanKH99 <98635201+AbdElRahmanKH99@users.noreply.github.com>
2024-04-12 07:40:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import string
-message = input("Enter the message: ")
-shift_num = int(input("Enter the shift number: "))
-def decrypt(x,y):
-    decrypted_message = ""
+message = input("Enter a message: ")
+shift_num = int(input("Enter a shift number: "))
+def encrypt(x,y):
+    encrypted_message = ""
     for letter in x:
         if letter in string.ascii_letters:
-            new_position = string.ascii_letters.index(letter)
-            original_position = (new_position - y) % 52
-            decrypted_message += string.ascii_letters[original_position]
+            original_position = string.ascii_letters.index(letter)
+            new_position = (original_position + y) % 52
+            encrypted_message += string.ascii_letters[new_position]
         else:
-            decrypted_message += letter
-    print(f"The encrypted message is:\n*****\n\n{decrypted_message}\n\n*****")
-decrypt(x = message, y = shift_num)
+            encrypted_message += letter
+    print(f"The encrypted message is:\n{encrypted_message}")
+encrypt(x = message, y = shift_num)
